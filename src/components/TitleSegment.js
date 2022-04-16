4a04f59caf635c5d28e2483874ce87a0c4dc07ee
Punya Shah <pzs5530@psu.edu>
2022-04-16 15:17:50 -0400

    Project2 completed commit

---
+++ src/components/TitleSegment.js
@@ -0,0 +1,19 @@
+// ./src/components/TitleSegment.js
+
+// Imports
+import React from "react";
+import { Typography, Select } from "antd";
+const { Title } = Typography;
+const { Option } = Select;
+
+// Title segment container
+export default function TitleSegment({}) {
+    // Contains the structure for the top section of the project
+
+    // Returns a JSX component for the title segment
+    return (
+        <>
+            <Title>SVG from a Decentralized Database Viewer</Title>
+        </>
+    );
+}
